5d1b9fca28bb0ad85287678a58a370fc0eeb4d78
Chetan Dhotre <75079526+mrdc48@users.noreply.github.com>
2021-01-01 15:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from "react";
-import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Chetan dhotre</h1>
-      <h2>Full stack developer </h2>
-    </div>
-  );
+ React, { useState } from 'react';
+import './styles.css';
+ function App(){
+	const [playing, setPlaying] = useState(false);
+
+	const HEIGHT = 500;
+	const WIDTH = 500;
+
+	const start = () => {
+		setPlaying(true);
+		navigator.getUserMedia(
+			{
+				video: true,
+        audio: true,
+			},
+			(stream) => {
+				let video = document.getElementsByClassName('app__videoFeed')[0];
+				if (video) {
+					video.srcObject = stream;
+				}
+			},
+			(err) => console.error(err)
+		);
+	};
+
+	const stop = () => {
+		setPlaying(false);
+		let video = document.getElementsByClassName('app__videoFeed')[0];
+		video.srcObject.getTracks()[0].stop();
+	};
+
+	return (
+		<div style={{border: "2px solid #fff",boxShadow:"5px 5px 6x #ddd"}}>
+			<div className="app__container">
+				<video
+					height={HEIGHT}
+					width={WIDTH}
+					muted
+					autoPlay
+					className="app__videoFeed"
+				></video>
+			</div>
+			<div>
+				{playing ? (
+					<button onClick={stop}>Stop</button>
+				) : (
+					<button onClick={start}>Start</button>
+				)}
+			</div>
+		</div>
+	);
 }
+export default App;
+
+ 
